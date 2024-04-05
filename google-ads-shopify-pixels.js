866bae2e415185ce3b768efa59d171cef7a48fca
Jordan Lagan <jordanlagan000@gmail.com>
2024-04-05 19:13:13 -0400

    Create google-ads-shopify-pixels.js

---
+++ google-ads-shopify-pixels.js
@@ -0,0 +1,108 @@
+// Step 1. Add your Google Ads tag without HTML
+const script = document.createElement("script");
+script.setAttribute("src", "https://www.googletagmanager.com/gtag/js?id=AW-EXAMPLE");
+script.setAttribute("async", "");
+document.head.appendChild(script);
+
+window.dataLayer = window.dataLayer || [];
+function gtag() {
+  dataLayer.push(arguments);
+}
+gtag("js", new Date());
+gtag("config", "AW-EXAMPLE");
+
+// Step 2. Create the functions to get the event data from Shopify
+const DPEvents = {
+  getItemsFromLineItems(lineItems) {
+    let items = []
+    for (const item of lineItems) {
+      items.push({
+        item_id: item.variant.product.id,
+        item_name: item.variant.product.title,
+      })
+    }
+
+    return items
+  },
+
+  getItemsFromProductVariants(productVariants) {
+    let items = []
+    for (const item of productVariants) {
+      items.push({
+        item_id: item.id,
+        item_name: item.product.title,
+      })
+    }
+
+    return items
+  },
+
+  getViewItemData(evt) {
+    return {
+      currency: evt.data.productVariant.price.currencyCode,
+      value: evt.data.productVariant.price.amount,
+      items: [{ item_id: evt.data.productVariant.id, item_name: evt.data.productVariant.product.title }],
+    }
+  },
+
+  getViewItemListData(evt) {
+    return {
+      items: this.getItemsFromProductVariants(evt.data.collection.productVariants),
+    }
+  },
+
+  getAddToCartData(evt) {
+    return {
+      currency: evt.data.cartLine.merchandise.price.currencyCode,
+      value: evt.data.cartLine.merchandise.price.amount,
+      items: [{ item_id: evt.data.cartLine.merchandise.id, item_name: evt.data.cartLine.merchandise.product.title }],
+    }
+  },
+
+  enhancedConversionData(evt) {
+    return {
+      email: evt.data.checkout.email,
+      phone_number: evt.data.checkout.phone,
+      address: {
+        first_name: evt.data.checkout.shippingAddress.firstName,
+        last_name: evt.data.checkout.shippingAddress.lastName,
+        street: evt.data.checkout.shippingAddress.address1,
+        city: evt.data.checkout.shippingAddress.city,
+        region: evt.data.checkout.shippingAddress.provinceCode,
+        postal_code: evt.data.checkout.shippingAddress.zip,
+        country: evt.data.checkout.shippingAddress.countryCode,
+      },
+    }
+  },
+
+  getCheckoutCompletData(evt) {
+    return {
+      send_to: "AW-1027764777/nPfVCJmFkAIQqeSJ6gM",
+      transaction_id: evt.data.checkout.order.id,
+      currency: evt.data.checkout.currencyCode,
+      value: evt.data.checkout.totalPrice.amount,
+      items: this.getItemsFromLineItems(evt.data.checkout.lineItems),
+    }
+  }
+}
+
+analytics.subscribe("checkout_completed", async (event) => {
+  gtag("set", "user_data", DPEvents.enhancedConversionData(event));
+  gtag("event", "conversion", DPEvents.getCheckoutCompletData(event));
+});
+
+analytics.subscribe("search_submitted", async (event) => {
+  gtag("event", "search", { search_term: event.data.searchResult.query, });
+});
+
+analytics.subscribe("product_viewed", async (event) => {
+  gtag("event", "view_item", DPEvents.getViewItemData(event));
+});
+
+analytics.subscribe("collection_viewed", async (event) => {
+  gtag("event", "view_item_list", DPEvents.getViewItemListData(event));
+});
+
+analytics.subscribe("product_added_to_cart", async (event) => {
+  gtag("event", "add_to_cart", DPEvents.getAddToCartData(event));
+});
