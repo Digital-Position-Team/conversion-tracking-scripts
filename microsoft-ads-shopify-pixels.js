1e59263ce88774de94238228bc79e4e541363cbf
Jordan Lagan <jordanlagan000@gmail.com>
2024-04-06 11:43:55 -0400

    Update microsoft-ads-shopify-pixels.js

--- microsoft-ads-shopify-pixels.js
+++ microsoft-ads-shopify-pixels.js
@@ -1,6 +1,10 @@
-// Simple Microsoft Ads tracking base tag and purchase event
+// This example shows how to install a Microsoft Ads base pixel and purchase event
+// (You will need to make sure the goal configuration matches the values used here)
+// Replace UET ID with your own and 'conversion' with your goal's action
+
+// NOTE:
 // Only 'event' type goals in Microsoft Ads will work since code runs in a sandbox and destination url cannot be reliably tracked
-// For desintation URL set it to 'processing?completed=true' in the microsoft ads interface
+// UNTESTED: (For desintation URL set it to 'processing?completed=true' in the microsoft ads interface)
 
 
 (function(w,d,t,r,u){var f,n,i;w[u]=w[u]||[],f=function(){var o={ti:"EXAMPLE-ID", enableAutoSpaTracking: true};o.q=w[u],w[u]=new UET(o),w[u].push("pageLoad")},n=d.createElement(t),n.src=r,n.async=1,n.onload=n.onreadystatechange=function(){var s=this.readyState;s&&s!=="loaded"&&s!=="complete"||(f(),n.onload=n.onreadystatechange=null)},i=d.getElementsByTagName(t)[0],i.parentNode.insertBefore(n,i)})(window,document,"script","//bat.bing.com/bat.js","uetq");
@@ -8,8 +12,9 @@
 
 analytics.subscribe("checkout_completed", (event) => {
   uetq = uetq || [];
-  uetq.push('event', 'purchase',
+  uetq.push('event', 'conversion',
     {
+      'event_category': 'your_event_category',
       'revenue_value': event.data.checkout.totalPrice.amount,
       'currency' : event.data.checkout.currencyCode
     }
