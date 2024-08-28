519834c1abc1729e1c3a5e0d2875c79cd011ebdc
Jordan Lagan <jordanlagan000@gmail.com>
2024-08-28 12:49:56 -0400

    Update google-analytics-shopify-pixels.js

--- google-analytics-shopify-pixels.js
+++ google-analytics-shopify-pixels.js
@@ -12,7 +12,7 @@ function gtag() {
   dataLayer.push(arguments);
 }
 gtag("js", new Date());
-gtag("config", "G-EXAMPLE");
+gtag("config", "G-EXAMPLE", { send_page_view: false });
 
 const DPEvents = {
   getItemsFromLineItems(lineItems) {
